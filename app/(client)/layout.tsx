f09540ae5bace961946c1deb976a58aabbd83539
Huân Nguyễn <huannguyen@Huans-MacBook-Pro.local>
2024-10-06 23:16:23 +0700

    update layout for index

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -9,9 +9,7 @@ export default function RootLayout({
   return (
     <>
       <NavBar />
-      {/* <h1>
-      <FormattedMessage id="greeting" />
-    </h1> */}
+   
       <div className="bg-white dark:bg-gray-900 flex flex-col items-center justify-center py-8">
         {children}
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import HomePage from "./(client)/page";
+import Footer from "./components/footer";
+import NavBar from "./components/nav-bar";
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <>
+      <NavBar />
+
+      <div className="bg-white dark:bg-gray-900 flex flex-col items-center justify-center py-8">
+        <HomePage />
+      </div>
+      <Footer />
+    </>
+  );
 }
